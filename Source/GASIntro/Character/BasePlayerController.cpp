44775ebe86ae63b669a8652501ec4bdf2718a95b
liang yutang <957753243@qq.com>
2023-07-24 16:18:52 +0800

    角色基本移动控制

--- Source/GASIntro/Character/BasePlayerController.cpp
+++ Source/GASIntro/Character/BasePlayerController.cpp
@@ -3,3 +3,73 @@
 
 #include "BasePlayerController.h"
 
+#include "BaseCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void ABasePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasePlayerController::Move(const FInputActionValue& Value)
+{
+	const FVector2d MovementVector=Value.Get<FVector2d>();
+	MyPawn=GetPawn();
+	if (MyPawn)
+	{
+		MyPawn->AddMovementInput(MyPawn->GetActorForwardVector(),MovementVector.X);
+		MyPawn->AddMovementInput(MyPawn->GetActorRightVector(),MovementVector.Y);
+	}
+	
+}
+
+void ABasePlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2d LookVector=Value.Get<FVector2d>();
+	MyPawn=GetPawn();
+	if (MyPawn)
+	{
+		MyPawn->AddControllerYawInput(-LookVector.X);
+		MyPawn->AddControllerPitchInput(LookVector.Y);
+	}
+}
+
+void ABasePlayerController::OnJump(const FInputActionValue& Value)
+{
+	if (ABaseCharacter* BaseCharacter=Cast<ABaseCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0)))
+	{
+		BaseCharacter->Jump();
+	}
+}
+
+void ABasePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (const APlayerController* PlayerController=Cast<APlayerController>(this))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext,0);
+		}
+	}
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent=CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		if (IA_Move)
+		{
+			EnhancedInputComponent->BindAction(IA_Move,ETriggerEvent::Triggered,this,&ABasePlayerController::Move);
+		}
+		if (IA_Jump)
+		{
+			EnhancedInputComponent->BindAction(IA_Jump,ETriggerEvent::Triggered,this,&ABasePlayerController::OnJump);
+		}
+		if (IA_Look)
+		{
+			EnhancedInputComponent->BindAction(IA_Look,ETriggerEvent::Triggered,this,&ABasePlayerController::Look);
+		}
+	}
+}
--- Source/GASIntro/Character/BasePlayerController.h
+++ Source/GASIntro/Character/BasePlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "BasePlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +16,32 @@ UCLASS()
 class GASINTRO_API ABasePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere)
+	APawn* MyPawn;
 	
+protected:
+	virtual void BeginPlay() override;
+	
+	void Move(const FInputActionValue &Value);
+
+	void Look(const FInputActionValue &Value);
+
+	void OnJump(const FInputActionValue &Value);
+	
+public:
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input|Context",meta=(AllowPrivateAccess="true"))
+	const UInputMappingContext* DefaultMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input|Action",meta=(AllowPrivateAccess="true"))
+	UInputAction* IA_Move;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input|Action",meta=(AllowPrivateAccess="true"))
+	UInputAction* IA_Jump;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Input|Action",meta=(AllowPrivateAccess="true"))
+	UInputAction* IA_Look;
 };
